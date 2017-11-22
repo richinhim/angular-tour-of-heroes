d78f9256933f4ad6bb3ed25c49e40b959588c593
sooyon <kwlsy666@naver.com>
2017-11-22 15:27:34 +0900

    The Hero List & Hero Details - Hero Details 를 따로 모듈화 처리 함. -> Hero Data를 Service로 개발

--- src/app/heores/heores.component.ts
+++ src/app/heores/heores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heores',
@@ -19,11 +20,17 @@ export class HeoresComponent implements OnInit {
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = this.heroes);
   }
 }
