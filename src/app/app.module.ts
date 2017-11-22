bce5f2d94c2e5dc0f287c4d38971c5cf1f4b9dc8
sooyon <kwlsy666@naver.com>
2017-11-22 14:35:46 +0900

    The Hero List & Hero Details - Hero Details 를 따로 모듈화 처리 함.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeoresComponent } from './heores/heores.component';
 import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeoresComponent
+    HeoresComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
